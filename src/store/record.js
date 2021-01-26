85faa34af02c22760996276f2b15bb3bec0bb7b4
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-01-26 15:49:25 +0600

    Implemented details of a record
    Implemented details of a record

--- src/store/record.js
+++ src/store/record.js
@@ -19,6 +19,16 @@ export default {
       } catch(e) {
         commit('setError', e)
       }
+    },
+    async fetchRecordById({ dispatch, commit }, id) {
+      try {
+        const uid = await dispatch('getUid')
+        const record = (await firebase.database().ref(`/users/${uid}/records`).child(id).once('value')).val() || {}
+        return {...record, id}
+      } catch(e) {
+        commit('setError', e)
+        throw e
+      }
     }
   }
 }
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,21 +1,31 @@
 <template>
   <div>
-    <div>
+    <Loader v-if="loading"/>
+    <p class="center" v-else-if="!record">
+      Record with id = {{ $route.params.id }} not exist
+    </p>
+    <div v-else>
       <div class="breadcrumb-wrap">
-        <a href="/history" class="breadcrumb">History</a>
-        <a class="breadcrumb">
-          Expense
+        <router-link to="/history" class="breadcrumb">History</router-link>
+        <a @click.prevent class="breadcrumb">
+          {{ record.type === 'income' ? 'Income' : 'Outcome' }}
         </a>
       </div>
       <div class="row">
         <div class="col s12 m6">
-          <div class="card red">
+          <div 
+            class="card"
+            :class="{
+              red: record.type === 'outcome',
+              green: record.type === 'income'
+            }"
+          >
             <div class="card-content white-text">
-              <p>Description:</p>
-              <p>Sum:</p>
-              <p>Category:</p>
+              <p>Description: {{ record.description }}</p>
+              <p>Sum: {{ record.amount | currency('KZT') }}</p>
+              <p>Category: {{ record.categoryName }}</p>
 
-              <small>12.12.12</small>
+              <small>{{ record.date | date('datetime') }}</small>
             </div>
           </div>
         </div>
@@ -23,3 +33,26 @@
     </div>
   </div>  
 </template>
+
+<script>
+export default {
+  data: () => ({
+    record: null,
+    loading: true
+  }),
+  async mounted() {
+    try {
+      const id = this.$route.params.id
+      const record = await this.$store.dispatch('fetchRecordById', id)
+      const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)     
+      
+      this.record = {
+        ...record,
+        categoryName: category.title
+      }      
+    } catch(e) { }
+
+    this.loading = false
+  }
+}
+</script>
