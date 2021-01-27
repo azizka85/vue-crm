27debdf76f410156b0ab9ca5178ebf3ed9bf4dbb
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-01-27 22:47:20 +0600

    Implemented pagination
    Implemented pagination

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import Loader from '@/components/app/Loader'
 import router from './router'
@@ -24,6 +25,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 firebase.initializeApp(firebaseConfig)
 
--- src/views/History.vue
+++ src/views/History.vue
@@ -16,29 +16,41 @@
     </p>
 
     <section v-else>
-      <HistoryTable :records="records" />
+      <HistoryTable :records="items" />
+
+      <Paginate 
+        v-model="page"
+        :page-count="pageCount"
+        :prev-text="'Prev'"
+        :next-text="'Next'"
+        :click-handler="moveToPage"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
     </section>
   </div>  
 </template>
 
 <script>
+import paginationMixin from '@/mixins/pagination.mixin.js'
 import HistoryTable from '@/components/HistoryTable'
 
 export default {
+  mixins: [paginationMixin],
   data: () => ({
     loading: true,
-    records: [],
-    categories: []
+    records: []
   }),
   async mounted() {
-    const records = await this.$store.dispatch('fetchRecords')
-    this.categories = await this.$store.dispatch('fetchCategories')
-    this.records = records.map(record => ({
+    this.records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
+
+    this.setupPagination(this.records.map(record => ({
       ...record, 
-      categoryName: this.categories.find(category => category.id === record.categoryId).title,
+      categoryName: categories.find(category => category.id === record.categoryId).title,
       typeClass: record.type === 'income' ? 'green' : 'red',
       typeText: record.type === 'income' ? 'Income' : 'Outcome'
-    }))
+    })))
     
     this.loading = false
   },
